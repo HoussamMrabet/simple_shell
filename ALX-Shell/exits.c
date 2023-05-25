87dc6738dbf4f15c3d1fcf66d1272f815f02f8d4
HoussamMrabet <dodslim100@gmail.com>
2023-05-25 10:00:20 +0100

    shell exit functionality

---
+++ ALX-Shell/exits.c
@@ -0,0 +1,74 @@
+#include "shell.h"
+
+/**
+ **copy_string - copies a string
+ *@dest: the destination string to be copied to
+ *@src: the source string
+ *@n: the amount of characters to be copied
+ *Return: the concatenated string
+*/
+char *copy_string(char *dest, char *src, int n)
+{
+    int i, j;
+    char *result = dest;
+
+    i = 0;
+    while (src[i] != '\0' && i < n - 1)
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    if (i < n)
+    {
+        j = i;
+        while (j < n)
+        {
+            dest[j] = '\0';
+            j++;
+        }
+    }
+    return result;
+}
+
+/**
+ **concatenate_strings - concatenates two strings
+ *@dest: the first string
+ *@src: the second string
+ *@n: the amount of bytes to be maximally used
+ *Return: the concatenated string
+*/
+char *concatenate_strings(char *dest, char *src, int n)
+{
+    int i, j;
+    char *result = dest;
+
+    i = 0;
+    j = 0;
+    while (dest[i] != '\0')
+        i++;
+    while (src[j] != '\0' && j < n)
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    if (j < n)
+        dest[i] = '\0';
+    return result;
+}
+
+/**
+ **find_character - locates a character in a string
+ *@s: the string to be parsed
+ *@c: the character to look for
+ *Return: (s) a pointer to the memory area s
+*/
+char *find_character(char *s, char c)
+{
+    do {
+        if (*s == c)
+            return s;
+    } while (*s++ != '\0');
+
+    return NULL;
+}

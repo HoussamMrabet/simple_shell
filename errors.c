ca4ba172e747da1804a8daa9e2e9e402b414c0e2
Mahmoud-Khairi <khairimahmoud01@gmail.com>
2023-05-25 13:04:17 +0100

    updating code

--- errors.c
+++ errors.c
@@ -8,14 +8,14 @@
  */
 void _eputs(char *str)
 {
-	int i = 0;
+	int x = 0;
 
 	if (!str)
 		return;
-	while (str[i] != '\0')
+	while (str[x] != '\0')
 	{
-		_eputchar(str[i]);
-		i++;
+		_eputchar(str[x]);
+		x++;
 	}
 }
 
@@ -28,16 +28,16 @@ void _eputs(char *str)
  */
 int _eputchar(char c)
 {
-	static int i;
+	static int a;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || a >= WRITE_BUF_SIZE)
 	{
-		write(2, buf, i);
-		i = 0;
+		write(2, buf, a);
+		a = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[a++] = c;
 	return (1);
 }
 
@@ -51,16 +51,16 @@ int _eputchar(char c)
  */
 int _putfd(char c, int fd)
 {
-	static int i;
+	static int y;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (c == BUF_FLUSH || y >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
-		i = 0;
+		write(fd, buf, y);
+		y = 0;
 	}
 	if (c != BUF_FLUSH)
-		buf[i++] = c;
+		buf[y++] = c;
 	return (1);
 }
 
@@ -73,13 +73,13 @@ int _putfd(char c, int fd)
  */
 int _putsfd(char *str, int fd)
 {
-	int i = 0;
+	int x = 0;
 
 	if (!str)
 		return (0);
 	while (*str)
 	{
-		i += _putfd(*str++, fd);
+		x += _putfd(*str++, fd);
 	}
-	return (i);
+	return (x);
 }

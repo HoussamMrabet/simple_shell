4d8ad2d1560568291e2b48e4d1fe6ecb3ebe4c80
HoussamMrabet <dodslim100@gmail.com>
2023-05-26 18:28:16 +0100

    set env and errors

--- 0-errors.c
+++ 0-errors.c
@@ -5,7 +5,7 @@
  * @str: the string to be printed
  *
  * Return: Nothing
- */
+*/
 void _eputs(char *str)
 {
 	int i = 0;
@@ -20,58 +20,60 @@ void _eputs(char *str)
 }
 
 /**
- * _eputchar - writes the character c to stderr
- * @c: The character to print
+ * _eputchar - writes the character ch to stderr
+ * @ch: The character to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- */
-int _eputchar(char c)
+*/
+int _eputchar(char ch)
 {
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (ch == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
 		write(2, buf, i);
 		i = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (ch != BUF_FLUSH)
+		buf[i++] = ch;
+
 	return (1);
 }
 
 /**
- * _putfd - writes the character c to given fd
- * @c: The character to print
- * @fd: The filedescriptor to write to
+ * _putfd - writes the character ch to given folder
+ * @ch: The character to print
+ * @folder: The filedescriptor to write to
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
- */
-int _putfd(char c, int fd)
+*/
+int _putfd(char ch, int folder)
 {
 	static int i;
 	static char buf[WRITE_BUF_SIZE];
 
-	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	if (ch == BUF_FLUSH || i >= WRITE_BUF_SIZE)
 	{
-		write(fd, buf, i);
+		write(folder, buf, i);
 		i = 0;
 	}
-	if (c != BUF_FLUSH)
-		buf[i++] = c;
+	if (ch != BUF_FLUSH)
+		buf[i++] = ch;
+
 	return (1);
 }
 
 /**
  * _putsfd - prints an input string
  * @str: the string to be printed
- * @fd: the filedescriptor to write to
+ * @folder: the filedescriptor to write to
  *
  * Return: the number of chars put
- */
-int _putsfd(char *str, int fd)
+*/
+int _putsfd(char *str, int folder)
 {
 	int i = 0;
 
@@ -79,7 +81,8 @@ int _putsfd(char *str, int fd)
 		return (0);
 	while (*str)
 	{
-		i += _putfd(*str++, fd);
+		i += _putfd(*str++, folder);
 	}
+
 	return (i);
 }
